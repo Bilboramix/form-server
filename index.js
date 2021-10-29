29d6130a79eb5cdcc7770adaef604f8ece0e311b
bilbo <axel.catois@gmail.com>
2021-10-29 17:44:58 +0200

    Working server with mailgun

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+require("dotenv").config();
+const mailgun = require("mailgun-js");
+const DOMAIN = process.env.MAILGUN_DOMAIN;
+const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN });
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+app.post("/form", (req, res) => {
+  try {
+    const data = {
+      from: `${req.fields.firstname} ${req.fields.lastname} ${req.fields.email}`,
+      to: String(process.env.MY_MAIL),
+      subject: `${req.fields.subject}`,
+      text: `${req.fields.message}`,
+    };
+    mg.messages().send(data, function (error, body) {
+      res.json(body);
+      if (error) {
+        res.json({ error: error });
+      }
+    });
+  } catch (error) {
+    res.json({ error: { message: error.message } });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("server ON");
+});
